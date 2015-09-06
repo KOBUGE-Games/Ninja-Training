379019cddfaa3dd8a6c83524c061dce290e17fc9
Alket Rexhepi <alketii@gmail.com>
2015-09-06 18:43:44 +0200

    added 5 jumping sounds

--- scripts/ninja.gd
+++ scripts/ninja.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var side = 0
 var jump = false
 var score = 0
+var sound = 0
 
 func _ready():
 	set_process_input(true)
@@ -19,21 +20,22 @@ func _fixed_process(delta):
 		jump = true
 
 func _input(ev):
-	if ev.is_pressed() && !ev.is_echo():
-		if jump:
-			jump = false
-			score +=1
-			get_parent().get_node("gui/scores").set_text("SCORE: "+str(score))
-			if side == 0:
-				move(Vector2(0,-10))
-				side = 1
-				get_node("AnimatedSprite/AnimationPlayer").play("rotate")
-				get_node("AnimatedSprite").set_flip_h(true)
-			else:
-				move(Vector2(0,10))
-				side = 0
-				get_node("AnimatedSprite/AnimationPlayer").play("rotate",-1,-1,true)
-				get_node("AnimatedSprite").set_flip_h(false)
+	if ev.is_pressed() && !ev.is_echo() && jump:
+		jump = false
+		sound = (randi() % 5)+1
+		get_node("SamplePlayer2D").play("hit"+str(sound))
+		score +=1
+		get_parent().get_node("gui/scores").set_text("SCORE: "+str(score))
+		if side == 0:
+			move(Vector2(0,-10))
+			side = 1
+			get_node("AnimatedSprite/AnimationPlayer").play("rotate")
+			get_node("AnimatedSprite").set_flip_h(true)
+		else:
+			move(Vector2(0,10))
+			side = 0
+			get_node("AnimatedSprite/AnimationPlayer").play("rotate",-1,-1,true)
+			get_node("AnimatedSprite").set_flip_h(false)
 
 func _on_VisibilityNotifier2D_exit_viewport( viewport ):
 	get_parent().get_node("gui/replay").show()
