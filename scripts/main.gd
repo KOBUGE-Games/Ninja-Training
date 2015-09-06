d83613f1a4f0bae8f54fbc98d803932a78f1ce34
Alket Rexhepi <alketii@gmail.com>
2015-09-06 17:23:53 +0200

    some test

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,26 +4,40 @@ var block = load("res://scenes/block.xml")
 var new_block
 var side = 0
 var speed = 10
+var amount = 0
 
 func _ready():
 	randomize()
+	initial_blocks()
 	get_node("gui/replay").hide()
 
 func _on_Timer_timeout():
-	new_block = block.instance()
-	side = randi() % 2
+	amount = (randi() % 4)+2
 	if side == 0:
-		new_block.set_pos(Vector2(1344,705))
-		new_block.get_node("Sprite").set_flip_v(false)
+		for i in range(amount):
+			new_block = block.instance()
+			new_block.set_pos(Vector2(1086+(i*128),705))
+			new_block.get_node("Sprite").set_flip_v(false)
+			new_block.speed = speed
+			add_child(new_block)
+		side = 1
 	else:
-		new_block.set_pos(Vector2(1344,62))
-		new_block.get_node("Sprite").set_flip_v(true)
-	
-	new_block.speed = speed
-	add_child(new_block)
+		for i in range(amount):
+			new_block = block.instance()
+			new_block.set_pos(Vector2(1086+(i*128),62))
+			new_block.get_node("Sprite").set_flip_v(true)
+			new_block.speed = speed
+			add_child(new_block)
+		side = 0
 
 func _on_replay_pressed():
 	get_tree().reload_current_scene()
 
 func _on_Timer_block_timeout():
 	speed += 1
+	
+func initial_blocks():
+	for i in range(15):
+		new_block = block.instance()
+		new_block.set_pos(Vector2(i*128,705))
+		add_child(new_block)
