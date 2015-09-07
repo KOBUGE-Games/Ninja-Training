bb7a8dec9d2faeb9dc9d7d29f6d512acb5a497bf
Alket Rexhepi <alketii@gmail.com>
2015-09-07 14:52:21 +0200

    a fix, still 2 bugs

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,6 +6,7 @@ var side = 0
 var speed = 10
 var amount = 0
 var last_i = 0
+var last_block
 
 func _ready():
 	randomize()
@@ -21,6 +22,8 @@ func _on_Timer_timeout():
 			new_block.set_pos(Vector2(1086+(i*128),705))
 			new_block.get_node("Sprite").set_flip_v(false)
 			new_block.speed = speed
+			new_block.add_collision_exception_with(last_block)
+			last_block = new_block
 			add_child(new_block)
 			last_i = i
 		side = 1
@@ -29,24 +32,11 @@ func _on_Timer_timeout():
 			new_block = block.instance()
 			new_block.set_pos(Vector2(1086+(i*128),62))
 			new_block.get_node("Sprite").set_flip_v(true)
-			new_block.speed = speed
+			new_block.add_collision_exception_with(last_block)
+			last_block = new_block
 			add_child(new_block)
 			last_i = i
 		side = 0
-		
-	if side == 0:
-		last_i += 1
-		new_block = block.instance()
-		new_block.set_pos(Vector2(1086+(last_i*128+64),63))
-	
-	else:
-		last_i += 1
-		new_block = block.instance()
-		new_block.set_pos(Vector2(1086+(last_i*128+64),704))
-	
-	new_block.get_node("Sprite").hide()
-	new_block.last_one = true
-	add_child(new_block)
 
 func _on_replay_pressed():
 	get_tree().reload_current_scene()
@@ -57,6 +47,9 @@ func _on_Timer_block_timeout():
 func initial_blocks():
 	for i in range(15):
 		new_block = block.instance()
-		new_block.set_pos(Vector2(i*128,705))
+		new_block.set_pos(Vector2(i*128+1,705))
+		if last_block != null:
+			new_block.add_collision_exception_with(last_block)
+		last_block = new_block
 		new_block.movement = false
 		add_child(new_block)
--- scripts/ninja.gd
+++ scripts/ninja.gd
@@ -6,15 +6,17 @@ var score = 0
 var sound = 0
 var start = false
 var dead = false
+var hit = false
 
 func _ready():
 	set_process_input(true)
 	set_fixed_process(true)
 	get_node("AnimatedSprite/AnimationPlayer").play("idle")
+	get_node("RayCast2D").add_exception(self)
 	
 func _fixed_process(delta):
 	if !dead:
-		if !is_colliding():
+		if !get_node("ray_bottom").is_colliding() && !get_node("ray_up").is_colliding():
 			jump = false
 			if side == 0:
 				move(Vector2(0,10))
@@ -22,11 +24,23 @@ func _fixed_process(delta):
 				move(Vector2(0,-10))
 		else:
 			jump = true
+
 	else:
 		if side == 0:
 			move(Vector2(0,10))
 		else:
 			move(Vector2(0,-10))
+	
+	if get_node("RayCast2D").is_colliding():
+		add_collision_exception_with(get_node("RayCast2D").get_collider())
+		hit = true
+		
+	if hit:
+		if side == 0:
+			move(Vector2(-int(get_node("RayCast2D").get_collider().speed),10))
+		else:
+			move(Vector2(-int(get_node("RayCast2D").get_collider().speed),-10))
+		hit = false
 
 func _input(ev):
 	if ev.is_pressed() && !ev.is_echo() && jump && start:
